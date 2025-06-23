0cf4b1fc83dfcdb211876d700f5e72f7028dc06f
Jorge Cristo <dev.jorge.c@gmail.com>
2025-06-23 11:45:05 +0000

    ok estou esperando o resultado do build aqui. mas ainda vejo o 404 aqui

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+
 const nextConfig = {
   output: 'export',
-  basePath: '/finecoding',
-  assetPrefix: '/finecoding/',
+  basePath: isGithubActions ? '/finecoding' : '',
+  assetPrefix: isGithubActions ? '/finecoding/' : '',
   images: {
     unoptimized: true,
   },
