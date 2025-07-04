f872359843461fd19eec8063c17669b0c1e4fa59
Jorge Cristo <dev.jorge.c@gmail.com>
2025-07-04 00:25:22 +0000

    Ops parece que o titulo da página tbm precisa de correção . Pfv garanta

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -52,7 +52,7 @@ export function Header() {
 
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 py-4 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background py-4">
       <div className="container flex max-w-screen-2xl flex-col items-center">
         {/* ROW 1: Title */}
         <div className="mb-4 flex items-center justify-center">
